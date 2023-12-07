bee4a05d81c092dc97c4f71244475f1b244f1e77
Bruno Dias <onurb.braz@gmail.com>
2023-12-06 23:24:53 -0300

    Rotas Ok

---
+++ Nosso-Clube/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import routes from './routes'
+import Router from 'vue-router'
+
+Vue.config.productionTip = false
+
+Vue.use(Router)
+
+const router = new Router({
+  routes,
+  mode: 'history'
+})
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
