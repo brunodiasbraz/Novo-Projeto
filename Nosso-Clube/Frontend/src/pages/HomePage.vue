5930a303f574e9de3079704e24a48fb19725c5cc
Bruno Dias <onurb.braz@gmail.com>
2023-12-07 17:15:35 -0300

    .

--- Nosso-Clube/Frontend/src/pages/HomePage.vue
+++ Nosso-Clube/Frontend/src/pages/HomePage.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <div>
-      <b-carousel
+  <div class="p-4">
+    
+      <div class="container-fluid px-5 row align-items-center">
+        <div class="col">
+            <div class="col " style="padding-left: 15%;">
+                <h1>A solução ideal para sua empresa</h1>
+                <p class="mt-4 mb-4 fs-5 col-9"><strong>Simplifique</strong> a comunicação entre seus atendentes e clientes.</p>
+                <a class="btn btn-outline-primary btn-lg" role="button" href="#" style="width: 180px; height: 50px">Saiba mais!</a>
+            </div>
+        </div>
+        <div class="col">
+            <img class="img-fluid" src="../assets/Teste.png" alt="aqui uma imagem">
+        </div>
+    
+
+
+      <!---<b-carousel
       id="carousel-fade"
       style="text-shadow: 0px 0px 2px #000"
       fade
@@ -21,7 +35,7 @@
       caption="Third Slide"
       img-src="https://picsum.photos/1024/480/?image=22"
     ></b-carousel-slide>
-  </b-carousel>
+  </b-carousel>-->
 </div>
   </div>
 </template>
