bae642f1eaec46f7c717b06a26312e8f6df1ff23
alexandrepsjf <alexandrepsjf@hotmail.com>
2024-02-03 23:45:29 -0300

    feat: config email envio e validação

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,16 +1,177 @@
+require('dotenv').config()
 const express = require("express");
 const routes = express.Router();
-
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const validator = require("email-validator");
+const nodemailer = require("nodemailer");
 const users = [{
     id: 1,
     name: 'Fideliza',
-    email: 'fidelizaapp@gmail.com',
+    email: 'fidelizaroutes@gmail.com',
     password: 12345,
 }];
 
-routes.get('/', (req, res) => {
-    res.send('Fideliza')
+
+const User = require("./models/User");
+
+
+// Open Route
+routes.get("/", (req, res) => {
+  res.status(200).json({ msg: "Bem vindo a API!" });
 });
+
+// Private Route
+routes.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  // check if user exists
+  const user = await User.findById(id, "-password");
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  res.status(200).json({ user });
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ msg: "Acesso negado!" });
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: "O Token é inválido!" });
+  }
+}
+
+routes.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmpassword } = req.body;
+
+  // validations
+  if (!name) {
+    return res.status(422).json({ msg: "O nome é obrigatório!" });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+  if (!validator.validate(email)) {
+    return res.status(422).json({ msg: "Digite um email válido!" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória!" });
+  }
+
+  if (password != confirmpassword) {
+    return res
+      .status(422)
+      .json({ msg: "A senha e a confirmação precisam ser iguais!" });
+  }
+
+  // check if user exists
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    return res.status(422).json({ msg: "Por favor, utilize outro e-mail!" });
+  }
+
+  // create password
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+  const sendEmail = process.env.EMAIL;
+  const  emailPassword = process.env.EMAIL_PASSWORD;
+  // create user
+  const user = new User({
+    name,
+    email,
+    passwordHash,
+  });
+
+  try {
+    await user.save();
+
+    res.status(201).json({ msg: "Usuário criado com sucesso!" });
+
+    let message = {
+        from: "gabusoftapp@gmail.com",
+        to: "alexandregabu@gmail.com",
+        subject: "Message title",
+        text: "Plaintext version of the message",
+        html: "<p>HTML version of the message</p>",
+    }
+
+    let transporter = nodemailer.createTransport({
+         host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // use TLS
+        auth: {
+          user: sendEmail,
+          pass: emailPassword,
+        },
+        connectionTimeout: 5000, // Ajuste o valor conforme necessário
+      });
+      transporter.sendMail(message).then(console.log('enviado com sucesso')).catch(console.error);
+
+
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+});
+
+routes.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // validations
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória!" });
+  }
+
+  // check if user exists
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  // check if password match
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+
+    res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+});
+
+
+
 
 routes.post('/login', (req, res) => {
     const { email, password } = req.body;
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+require('dotenv').config()
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const req = require("express/lib/request");
 const routes = require('./routes');
 const app = express();
@@ -6,6 +10,29 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Ouvindo a porta 3000')
-})
+
+const dbUser = process.env.DB_USER;
+const  dbPassword = process.env.DB_PASS;
+
+mongoose.connect(
+    `mongodb+srv://${dbUser}:Nosso_clube2024@nossoclube.d7xa57q.mongodb.net/?retryWrites=true&w=majority`
+  ).then(() => {
+    console.log("Conectou ao banco!");
+    app.listen(3000);
+  }).catch((err) => console.log(err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
