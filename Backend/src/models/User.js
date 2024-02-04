c49fc932c2655674f0797285a0c54c65fecbe8be
alexandrepsjf <alexandrepsjf@hotmail.com>
2024-02-04 20:00:36 -0300

    feat: verificaçõ de email de cadsatro

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -4,6 +4,8 @@ const User = mongoose.model('User', {
   name: String,
   email: String,
   password: String,
+  email_verify: Date,
+  token_verify: String,
 })
 
 module.exports = User
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -6,35 +6,15 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const validator = require("email-validator");
 const nodemailer = require("nodemailer");
-const users = [{
-    id: 1,
-    name: 'Fideliza',
-    email: 'fidelizaroutes@gmail.com',
-    password: 12345,
-}];
+const { uid, suid } = require('rand-token');
+
+
+
 
 
 const User = require("./models/User");
 
 
-// Open Route
-routes.get("/", (req, res) => {
-  res.status(200).json({ msg: "Bem vindo a API!" });
-});
-
-// Private Route
-routes.get("/user/:id", checkToken, async (req, res) => {
-  const id = req.params.id;
-
-  // check if user exists
-  const user = await User.findById(id, "-password");
-
-  if (!user) {
-    return res.status(404).json({ msg: "Usuário não encontrado!" });
-  }
-
-  res.status(200).json({ user });
-});
 
 function checkToken(req, res, next) {
   const authHeader = req.headers["authorization"];
@@ -52,6 +32,51 @@ function checkToken(req, res, next) {
     res.status(400).json({ msg: "O Token é inválido!" });
   }
 }
+
+
+// Private Route
+routes.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  // check if user exists
+  const user = await User.findById(id, "-password");
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  res.status(200).json({ user });
+});
+
+// Private Route
+
+// Open Route
+routes.get("/", (req, res) => {
+  res.status(200).json({ msg: "Bem vindo a API!" });
+
+});
+
+
+routes.post("/auth/verify", async (req, res) => {
+  const { token_verify} = req.body;
+console.log(token_verify)
+ // check if user exists
+ const userExists = await User.findOne({ token_verify: token_verify });
+
+ if (!userExists) {
+   return res.status(422).json({ msg: "Esse link expirou ou não é válido!" });
+ }
+
+ userExists.email_verify=Date();
+ userExists.token_verify=null;
+ try {
+  await userExists.save();
+
+  res.status(201).json({ msg: "Seu email foi verificado, Você será direcionado para a tela de Login" });
+ } catch (error) {
+  console.log(error);
+ }
+});
 
 routes.post("/auth/register", async (req, res) => {
   const { name, email, password, confirmpassword } = req.body;
@@ -96,19 +121,26 @@ routes.post("/auth/register", async (req, res) => {
     email,
     passwordHash,
   });
+  const verifyToken = suid(16);
+  user.token_verify=verifyToken;
 
+  console.log(verifyToken)
   try {
     await user.save();
 
-    res.status(201).json({ msg: "Usuário criado com sucesso!" });
+    res.status(201).json({ msg: "Usuário criado com sucesso! Verifique sua caixa de email e faça a validação do seu cadastro" });
 
     let message = {
         from: "gabusoftapp@gmail.com",
-        to: "alexandregabu@gmail.com",
-        subject: "Message title",
+        to: user.email,
+        subject: "Validação de cadastro Nosso Clube",
         text: "Plaintext version of the message",
-        html: "<p>HTML version of the message</p>",
-    }
+        html:  '<img src="https://ci3.googleusercontent.com/meips/ADKq_NYL2HBvmfArevX3NVujmQCPWNFsgX3e2hjNCIZn7wwvXIa1forX93ezrWp2zlocURZiHNdcmUKX7nVR1hykvEYH9z2V1PNazvd4tbOmYD4KRmNam4z_uunMuZD-cp4vJl0hboEc_C7xPxOvlJ9qU6pUjTiS2cwgesI=s0-d-e1-ft#https://mcusercontent.com/80734d74fa766a626186188dc/images/64dbc363-0c78-fb86-2e2f-a7e49b5615f1.png" alt="Imagem Externa">'
+        +`<p>Olá, Por favor, clique no link abaixo para verificar seu endereço de e-mail:</p>`
+        +`<a href="https://seusite.com/verify-email?token=${verifyToken}">`
+        +`Verificar Email</a><p>Se você não solicitou esta verificação, ignore este e-mail.</p>`
+    ,
+}
 
     let transporter = nodemailer.createTransport({
          host: "smtp.gmail.com",
@@ -169,10 +201,6 @@ routes.post("/auth/login", async (req, res) => {
     res.status(500).json({ msg: error });
   }
 });
-
-
-
-
 routes.post('/login', (req, res) => {
     const { email, password } = req.body;
     const user = users.find(user => user.email === email && user.password === password);
@@ -183,5 +211,6 @@ routes.post('/login', (req, res) => {
 
     return res.status(401).json({ message: 'Credenciais invalidas'});
 })
+// Open Route
 
 module.exports = routes;
