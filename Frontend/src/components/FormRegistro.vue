7f075dc41817680972c9f81b9fb54dd31931ff6a
Bruno Bráz <79773641+brunodiasbraz@users.noreply.github.com>
2024-06-25 22:13:40 -0300

    Revert "Development"

--- Frontend/src/components/FormRegistro.vue
+++ Frontend/src/components/FormRegistro.vue
@@ -1,33 +1,41 @@
 <template>
-  <h1 class="mb-4 mt-2 font-weight-normal text-center">Registre-se</h1>
+  <h1 class="mb-4 font-weight-normal text-center">Registre-se</h1>
 
-  <form @submit.prevent="submitForm" class="d-grid gap-3">
+  <form class="col-10 d-grid gap-3">
     <div class="form-group">
       <label for="emailRegistro" class="sr-only">Seu email</label>
-      <input v-model="email" type="email" id="emailRegistro" class="form-control" placeholder="Seu email" required autofocus>
+      <input v-model="email" type="email" id="emailRegistro" class="form-control" placeholder="Seu email" required
+        autofocus>
       <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
     </div>
     <div class="form-group">
       <label for="passwordRegistro" class="sr-only">Sua senha</label>
-      <input v-model="password" type="password" class="form-control" id="passwordRegistro" placeholder="Senha" required>
+      <input type="password" class="form-control" id="passwordRegistro" placeholder="Senha">
     </div>
     <div class="form-group">
       <label for="confPasswordRegistro" class="sr-only">Confirme sua senha</label>
-      <input v-model="confirmPassword" type="password" id="confPasswordRegistro" class="form-control" placeholder="Confirme sua senha" required>
+      <input v-model="password" type="password" id="confPasswordRegistro" class="form-control"
+        placeholder="Confirme sua senha" required>
     </div>
-    <BtnRoxo class="col-12 mt-3" id="btnLogin" text="Registrar" @click="submitForm" />
-  </form>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
+    </div>
+    <BtnRoxo class="col-12 mt-3" id="btnLogin" type="submit" text="Registrar" @click="submitForm" />
 
+  </form>
   <!----------Alertas----------->
   <div class="mt-4 alert alert-danger alert-dismissible fade show d-none" id="alertError" role="alert">
     Ocorreu um problema durante o registro. Tente novamente mais tarde.
   </div>
   <div class="mt-4 alert alert-success alert-dismissible fade show d-none" id="alertSuccess" role="alert">
-    Um e-mail de confirmação foi enviado. <strong>Acesse seu e-mail e clique no link para continuar o cadastro.</strong>
+    Um e-mail de confirmação foi enviado. <strong>Acesse seu e-mail e clique no link para continuar o
+      cadastro.</strong>
   </div>
   <!----------Fim Alertas----------->
   <hr class="mt-4">
-  <p class="text-center">Já possui cadastro?<br> <a href="/login">Clique aqui</a> para fazer o login.</p>
+  <p class="text-center px-5">Já possui cadastro? <a href="/login">Clique aqui</a> para fazer o login.</p>
+
 </template>
 
 <script setup>
@@ -39,10 +47,8 @@ const email = ref('');
 const password = ref('');
 const confirmPassword = ref('');
 const name = ref('');
-const apiUrl = import.meta.env.VITE_API_URL;
 
 const submitForm = async () => {
-  //console.log('Entrei no submitForm'); 
   try {
     // Validar se os campos estão preenchidos
     if (!email.value || !password.value || !confirmPassword.value) {
@@ -55,36 +61,38 @@ const submitForm = async () => {
       console.error('As senhas não coincidem.');
       return;
     }
-    const response = await axios.post(`http://${apiUrl}/auth/register`, {
+
+    const response = await axios.post('http://localhost:3000/auth/register', {
       name: name.value,
       email: email.value,
       password: password.value,
       confirmpassword: confirmPassword.value,
     });
 
-    console.log('Response status:', response.status);
     if (response.status === 201) {
       // Exibir a div de sucesso
       document.getElementById('alertSuccess').classList.remove('d-none');
       setTimeout(() => {
-        document.getElementById('alertSuccess').classList.add('d-none');
+        alertSuccess.classList.add('d-none');
       }, 8000);
     } else {
       // Exibir a div de erro
       document.getElementById('alertError').classList.remove('d-none');
       setTimeout(() => {
-        document.getElementById('alertError').classList.add('d-none');
+        alertError.classList.add('d-none');
       }, 5000);
     }
   } catch (error) {
-    console.error('Erro ao enviar formulário:', error.message);
     document.getElementById('alertError').classList.remove('d-none');
     setTimeout(() => {
-      document.getElementById('alertError').classList.add('d-none');
+      alertError.classList.add('d-none');
     }, 5000);
+    //console.error('Erro ao enviar formulário 2:', error.message); // Adicione .message
   }
 };
+
 </script>
+
 
 <style scoped>
 a {
--- Frontend/src/views/VerifyEmail.vue
+++ Frontend/src/views/VerifyEmail.vue
@@ -1,101 +1,81 @@
 <template>
-  <div class="pt-5 text-center bg-bege">
-    <h1 class="col-12">Verificando Email...</h1>
-    <div class="col-12 mx-4 loader"></div>
-  </div>
+    <div class="row text-center py-5 justify-content-center bg-bege ">
+        <h1 class="col-12">Verificando Email...</h1>
+        <br />
+        <div class="col-12 loader"></div>
+
+    </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
-
+import axios from 'axios';
 export default {
-  setup() {
-    const router = useRouter()
-    const route = useRoute()
-    const token = ref(route.params.token)
-    const apiUrl = import.meta.env.VITE_API_URL;
 
-    //console.log('Token recebido:', token.value);
+    created() {
 
-    const verifyToken = async () => {
-      try {
-        console.log('Enviando token para verificação:', token.value)
-
-        const response = await fetch(`http://${apiUrl}/auth/verify`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ token_verify: token.value })
-        })
-
-        const data = await response.json()
-        console.log('Resposta do servidor:', data)
-
-        if (response.status === 201) {
-          console.log('Token válido. Redirecionando para /compregistro')
-          router.push({ path: '/compregistro', replace: true });
-        } else {
-          console.log('Token inválido. Redirecionando para /registrar')
-          //router.push({ path: '/registrar', replace: true });
+        const resposta = submitForm(this.$route.query.token)
+        console.log(resposta)
+        if(resposta.status==201){
+            router.push({ path: '/home', replace: true })
         }
-      } catch (error) {
-        console.error('Erro ao verificar o token:', error)
-        //router.push({ path: '/registrar', replace: true });
-      }
+
     }
 
-    onMounted(() => {
-      verifyToken()
-    })
+}
+const submitForm = (token) => {
+    try {
+        const response = axios.post('http://localhost:3000/auth/verify', {
 
-    return {
-      token
+            token_verify: token
+        });
+
+        return response;
+    } catch (error) {
+        console.error('Erro');
+        //console.error('Erro ao enviar formulário 2:', error.message); // Adicione .message
     }
-  }
 }
 </script>
 
 <style scoped>
 div {
-  height: 60vh;
+    height: 60vh;
 }
 
 .bg-bege {
-  background: #fbf7ec;
-  height: 100vh;
+    background: #FBF7EC;
+    /* Sombra desejada */
 }
 
+/* HTML: <div class="loader"></div> */
 .loader {
-  width: auto;
-  padding: 10px;
-  height: 22px;
-  border-radius: 40px;
-  color: #514b82;
-  border: 2px solid;
-  position: relative;
-  overflow: hidden;
+    width: 120px;
+    height: 22px;
+    border-radius: 40px;
+    color: #514b82;
+    border: 2px solid;
+    position: relative;
+    overflow: hidden;
 }
 
 .loader::before {
-  content: '';
-  position: absolute;
-  margin: 2px;
-  width: 14px;
-  top: 0;
-  bottom: 0;
-  left: -20px;
-  border-radius: inherit;
-  background: currentColor;
-  box-shadow: -10px 0 12px 3px currentColor;
-  clip-path: polygon(0 5%, 100% 0, 100% 100%, 0 95%, -30px 50%);
-  animation: l14 1s infinite linear;
+    content: "";
+    position: absolute;
+    margin: 2px;
+    width: 14px;
+    top: 0;
+    bottom: 0;
+    left: -20px;
+    border-radius: inherit;
+    background: currentColor;
+    box-shadow: -10px 0 12px 3px currentColor;
+    clip-path: polygon(0 5%, 100% 0, 100% 100%, 0 95%, -30px 50%);
+    animation: l14 1s infinite linear;
 }
 
 @keyframes l14 {
-  100% {
-    left: calc(100% + 20px);
-  }
+    100% {
+        left: calc(100% + 20px)
+    }
 }
 </style>
